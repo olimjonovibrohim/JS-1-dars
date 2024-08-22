99fed6a9f762e86a565eda2f5d38088ee61dcb8d
olimjonovibrohim <ib0130nik@gmail.com>
2024-08-22 13:17:36 +0500

    JS 1-dars
    Zeromax IT Center

---
+++ JS/main.js
@@ -0,0 +1,37 @@
+// JSga kirish
+//console.log("Men faqat konsol oynasida korinaman");
+//alert("Saytimizga Xush Kelibsiz!")
+//document.write("Men bodyda korinaman")
+
+// JS o`zgartiruvchilari
+
+// let x = 5;
+// let y = 7;
+// let z = 10;
+// x = 15;
+// let salom = "Assalomu alaykum!"
+// let rang = "#f1f1f1"
+// let x = 15 , y = 7 , z = 10 
+// document.write(x+y+z);
+
+// JS da malumotlar turlari
+
+// let son = 45;               // number
+// let matn = "JavaScript";    // string
+
+// document.write( typeof matn);
+
+// let a = 1120, b =507 , c =54 , d =10 , e = 923;
+// document.write(b+e-c*d+a);
+
+// JS da matematika 
+let x = 5;
+// x += 5;   x=5+5      qo`shish
+// x -= 5;   x=5-5      ayirish
+// x *= 5;   x=5*5      ko`paytirish
+// x /= 5;   x=5/5      bo`lish
+// let qoldiq = x%5;    qoldiq 
+// let daraja = x**5;   daraja
+
+
+document.write();
